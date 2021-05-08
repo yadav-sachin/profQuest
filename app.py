c8ef58ae7327fc5cb78026b54eab451444b96016
yadav-sachin <sachinyadav7024@gmail.com>
2021-05-09 03:05:02 +0530

    complete integration for subject search in app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from tfidf_model import process_query
+from tfidf_model import process_name_query
 
 app = Flask(__name__)
 
@@ -16,17 +17,33 @@ def home():
 
 @app.route("/search")
 def search_subject():
+    countries_list = []
+    with open("web_data/countries_list.txt") as country_file:
+        countries_list = [line.strip() for line in country_file]
+    institutions_list = []
+    for country in countries_list:
+        with open("web_data/{}_institutes.txt".format(country)) as institutes_file:
+            institutions_list.extend([line.strip() for line in institutes_file])
     if 'q' in request.args: 
         query = request.args['q']
-        pageSize = 1000
+        pageSize = 25
         if 'pagesize' in request.args:
             pageSize = request.args['pagesize']
         pageNum = 1
-        query_docs = process_query(query, request.args)
+        query_docs = []
+        isNameQuery = False
+        if request.args['searchType'] == 'subject':
+            query_docs = process_query(query, request.args)
+        else:
+            query_docs = process_name_query(query, request.args)
+            print(query_docs)
+            isNameQuery = True
         if 'page' in request.args:
             pageNum = int(request.args['page'])
         query_docs = query_docs[(pageNum - 1)*(pageSize) : (pageNum)*(pageSize)]
-        return render_template("search_results.html", query_docs = query_docs, pageNum = pageNum)
+        return render_template("search_results.html", query_docs = query_docs, pageNum = pageNum, countries = countries_list, institutes = institutions_list, request_args = request.args, isNameQuery = isNameQuery)
+    else:
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,29 +1,52 @@
-$(document).ready(function() { 
-    $uncheckedSiblings = $("input:checkbox:not(:checked)").siblings("input");
+$(document).ready(function () {
+    $uncheckedSiblings = $("input:checkbox:not(:checked)").siblings("div, input");
     $uncheckedSiblings.fadeOut();
     $("#tfidf_slider").fadeOut();
-
+    checkpoint_change_react(); 
+    
     selected_countries_list = [];
     $('.selectpicker').selectpicker();
+    if ($("#searchType-input").val() == "name")
+    {
+        $("#searhbox-container").removeClass("hue-rotate-background");
+        $(".slider-container").fadeOut();
+    }
+    else
+    {
+        $("#searhbox-container").addClass("hue-rotate-background");
+        $(".slider-container").fadeIn();
+    }
 
+    $("#searchType-input").change(function () {
+        if ($("#searchType-input").val() == "name")
+        {
+            $("#searhbox-container").removeClass("hue-rotate-background");
+            $(".slider-container").fadeOut();
+        }
+        else
+        {
+            $("#searhbox-container").addClass("hue-rotate-background");
+            $(".slider-container").fadeIn();
+        }
+    });
 });
 
-$(document).ready(function () { 
+$(document).ready(function () {
     $(".selectpicker").selectpicker();
     $(".bootstrap-select").click(function () {
-         $(this).addClass("open");
+        $(this).addClass("open");
     });
-  });
+});
 
 
 
-function checkpoint_change_react(){
-    $uncheckedSiblings = $(".additional-filter-criteria-checkbox:checkbox:not(:checked)").siblings("input");
+function checkpoint_change_react() {
+    $uncheckedSiblings = $(".additional-filter-criteria-checkbox:checkbox:not(:checked)").siblings("div, input");
     $uncheckedSiblings.fadeOut();
-    $checkedSiblings = $(".additional-filter-criteria-checkbox:checkbox:checked").siblings("input");
+    $checkedSiblings = $(".additional-filter-criteria-checkbox:checkbox:checked").siblings("div, input");
     if ($checkedSiblings.length > 0)
         $("#tfidf_slider").fadeIn();
-    else    
+    else
         $("#tfidf_slider").fadeOut();
     $checkedSiblings.fadeIn();
 }
